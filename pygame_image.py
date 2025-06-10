795ff7ec0a8871a54dfc30110a6e6ea9916adc6d
c0a24016ea <c0a24016ea@edu.teu.ac.jp>
2025-06-10 17:22:36 +0900

    問１

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        mv = [-1,0]
         if key_lst[pg.K_UP]:
             kk_rct.move_ip(0, -1)
         if key_lst[pg.K_DOWN]:
@@ -29,6 +30,8 @@ def main():
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip(+1, 0)
+        kk_rct.move_ip(mv)
+        
         
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
